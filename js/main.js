86ed71b6b385828bd81ed44e1e900e3f37061dd1
William Suarez <wsgwilliam2557@gmail.com>
2026-02-06 09:22:38 -0500

    Primer commit del portafolio

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+let lang="es";
+const t={
+ es:{
+  name:"William Suarez",
+  role:"Desarrollador Web Full Stack",
+  about_title:"Sobre mí",
+  about_text:"Soy desarrollador web Full Stack",
+  projects_title:"Proyectos",
+  project1:"Plataforma para servicios de mudanzas.",
+  contact_title:"Contacto",
+  send:"Enviar",
+  name_ph:"Nombre",
+  email_ph:"Correo",
+  message_ph:"Mensaje",
+  about_title1:"habilidades",
+  skills_title: "Habilidades",
+  btn_code: "Código",
+  btn_demo: "Ver Demo",
+   soft_skills_title: "Habilidades Blandas",
+  soft_communication: "Comunicación",
+  soft_teamwork: "Trabajo en equipo",
+  soft_problem: "Resolución de problemas",
+  soft_adaptability: "Adaptabilidad"
+  
+
+ },
+ en:{
+  name:"William Suarez",
+  role:"Full Stack Web Developer",
+  about_title:"About Me",
+  about_text:"I am a Full Stack Web Developer specialized in JavaScript, React and Node.js.",
+  projects_title:"Projects",
+  project1:"Platform for moving services.",
+  contact_title:"Contact",
+  send:"Send",
+  name_ph:"Name",
+  email_ph:"Email",
+  message_ph:"Message",
+  about_title1:"skils",
+  skills_title: "Skills",
+   btn_code: "Code",
+  btn_demo: "Live Demo",
+   soft_skills_title: "Soft Skills",
+  soft_communication: "Communication",
+  soft_teamwork: "Teamwork",
+  soft_problem: "Problem Solving",
+  soft_adaptability: "Adaptability"
+  
+
+ }
+};
+
+
+
+
+function render(){
+ document.querySelectorAll("[data-i18n]").forEach(e=>e.textContent=t[lang][e.dataset.i18n]);
+ document.querySelectorAll("[data-i18n-placeholder]").forEach(e=>e.placeholder=t[lang][e.dataset.i18nPlaceholder]);
+}
+document.getElementById("lang-btn").onclick=()=>{
+ lang=lang==="es"?"en":"es";
+ document.getElementById("lang-btn").textContent=lang==="es"?"EN":"ES";
+ render();
+};
+render();
